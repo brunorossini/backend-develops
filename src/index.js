5b8f796383e023b40ea9bd21605cfefcb8b04c85
bruno.rossini <brunorossini@live.com>
2019-08-26 09:44:22 -0300

    Repository started

---
+++ src/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+app.use("/.well-known", express.static(path.resolve(__dirname, ".well-known")));
+
+app.listen(8080, function() {
+  console.log("Example app listening on port 8080!");
+});
